1bad8df936e64cb911241f9df264773e976612af
Matt <Mletter1@unm.edu>
2020-05-28 20:28:07 -0600

    ts convertion

--- client/src/__tests__/App.test.tsx
+++ client/src/__tests__/App.test.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import * as React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import { mount } from 'enzyme';
+import App from '../App';
+import '@testing-library/jest-dom/extend-expect';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+  const newApp = mount(<App />);
+  expect(newApp).toMatchSnapshot();
 });
